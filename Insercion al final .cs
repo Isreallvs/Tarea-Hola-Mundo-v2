bf7fb05e27bf0759111a64b876b492d8f0b3dfb3
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-07 16:50:30 -0700

    despues de revisar como se hace la insercion al final agrego la insercion al final en c#

---
+++ Insercion al final .cs
@@ -0,0 +1,38 @@
+using System;
+class program
+{
+    static void Main(string [] args)
+    {
+        int [] arr = {11, 21, 31, 41, 51, 61};
+        
+        //El arreglo antes de la inserción
+        Console.WriteLine("Antes de la inserción el array es: ");
+
+        foreach (int elemento in arr)
+        {
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
+
+        //Insertamos el elemento al inicio
+        int ele = 52;
+        
+        //Hacemos un nuevo array
+        int [] nuevoArr = new int[arr.Length + 1];
+        
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            nuevoArr[i] = arr[i];
+        }
+        //Se mueve a la ultima posicion
+        nuevoArr[arr.Length] = ele;
+
+        //El arreglo despues de la inserción
+        Console.WriteLine("El array despues de la inserción es: ");
+        foreach (int elemento in nuevoArr)
+        {
+            Console.Write(elemento + " ");
+        }
+    }
+}
