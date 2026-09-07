57efa3b84921f6fc77b1775f411d53b6cd03348e
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-07 10:57:16 -0700

    basandome en la estructura de insercion al inicio, cambie el arr.unshift por arr.push

---
+++ Inserción al final.js
@@ -0,0 +1,8 @@
+const arr = [11, 21, 31, 41, 51, 61]
+var ele = 52
+console.log("Antes de la inserción el array es: ")
+console.log(arr.join(", "))
+//Inserción del elemento al final del array
+arr.push(ele)
+console.log("\nDespués de la inserción el array es: ")  
+console.log(arr.join(", "))
